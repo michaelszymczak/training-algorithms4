f06aaa266ee21fb9b38abd5d17a1e172fa1a90b5
Michael Szymczak <contact@michaelszymczak.com>
2015-08-30 16:24:12 +0100

    1_1_6 Done

---
+++ src/main/java/com/michaelszymczak/training/algorithms4/chapter01/section01/_1_1_5.java
@@ -0,0 +1,8 @@
+package com.michaelszymczak.training.algorithms4.chapter01.section01;
+
+public class _1_1_5 {
+
+    public static boolean areStrictlyBetweenZeroAndOne(double a, double b) {
+        return a >= 0.0 && a <= 1.0 && b >= 0.0 && b <= 1.0;
+    }
+}
---
+++ src/main/java/com/michaelszymczak/training/algorithms4/chapter01/section01/_1_1_6.java
@@ -0,0 +1,18 @@
+package com.michaelszymczak.training.algorithms4.chapter01.section01;
+
+public class _1_1_6 {
+
+    public static String numbers(int maxI) {
+        String result = "";
+        int f = 0;
+        int g = 1;
+        for (int i = 0; i <= maxI; i++)
+        {
+            result += f + ",";
+            f = f + g;
+            g = f - g;
+        }
+
+        return result;
+    }
+}
