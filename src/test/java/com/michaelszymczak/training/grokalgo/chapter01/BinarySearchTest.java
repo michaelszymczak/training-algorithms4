fcfd55ab1c85f338f3593c67cbe6167a0e2ca6a8
Michael Szymczak <contact@michaelszymczak.com>
2021-04-17 15:24:56 +0100

    #1 Binary search: empty array

--- src/test/java/com/michaelszymczak/training/grokalgo/chapter01/BinarySearchTest.java
+++ src/test/java/com/michaelszymczak/training/grokalgo/chapter01/BinarySearchTest.java
@@ -1,12 +1,25 @@
 package com.michaelszymczak.training.grokalgo.chapter01;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class BinarySearchTest
 {
     @Test
-    void nothing()
+    void shouldNotFindAnythingInAnEmptyArray()
     {
+        final BinarySearch binarySearch = new BinarySearch();
+        final int[] input = new int[0];
 
+        Assertions.assertThat(binarySearch.find(input, 3)).isEqualTo(-1);
+
+    }
+
+    private static final class BinarySearch
+    {
+        public int find(final int[] input, final int sought)
+        {
+            return -1;
+        }
     }
 }
