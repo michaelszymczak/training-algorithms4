66729d313ea1c763a47b39e2598600fed05db740
Michael Szymczak <contact@michaelszymczak.com>
2021-04-24 16:08:58 +0100

    #1 Naive sort: single element

--- src/test/java/com/michaelszymczak/training/grokalgo/chapter02/NaiveSortTest.java
+++ src/test/java/com/michaelszymczak/training/grokalgo/chapter02/NaiveSortTest.java
@@ -6,19 +6,35 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class NaiveSortTest
 {
+
+    private final NaiveSort sort = new NaiveSort();
+
     @Test
     void shouldResultInEmptyArrayWhenNoItems()
     {
-        NaiveSort sort = new NaiveSort();
         int[] result = sort.sort(new int[0]);
+
         assertThat(result).isEmpty();
+    }
+
+    @Test
+    void shouldReturnTheOnlyElement()
+    {
+        int[] result = this.sort.sort(new int[]{3});
+
+        assertThat(result).isEqualTo(new int[]{3});
     }
 
     private static final class NaiveSort
     {
         public int[] sort(final int[] input)
         {
-            return new int[0];
+            final int[] result = new int[input.length];
+            if (input.length != 0)
+            {
+                result[0] = input[0];
+            }
+            return result;
         }
     }
 }
