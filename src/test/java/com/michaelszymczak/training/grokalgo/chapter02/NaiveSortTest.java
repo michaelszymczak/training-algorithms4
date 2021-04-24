bcf5be43fe2130d7cbc37362bc601fa23f2b9cbd
Michael Szymczak <contact@michaelszymczak.com>
2021-04-24 16:05:52 +0100

    #1 Naive sort: empty case

--- src/test/java/com/michaelszymczak/training/grokalgo/chapter02/NaiveSortTest.java
+++ src/test/java/com/michaelszymczak/training/grokalgo/chapter02/NaiveSortTest.java
@@ -2,11 +2,23 @@ package com.michaelszymczak.training.grokalgo.chapter02;
 
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class NaiveSortTest
 {
     @Test
-    void nothing()
+    void shouldResultInEmptyArrayWhenNoItems()
     {
+        NaiveSort sort = new NaiveSort();
+        int[] result = sort.sort(new int[0]);
+        assertThat(result).isEmpty();
+    }
 
+    private static final class NaiveSort
+    {
+        public int[] sort(final int[] input)
+        {
+            return new int[0];
+        }
     }
 }
