e89c49ad6582025965484af7156edf14e54cc04d
Michael Szymczak <contact@michaelszymczak.com>
2021-05-01 17:10:08 +0100

    #1 BFS: continue working on the queue

---
+++ src/test/java/com/michaelszymczak/training/grokalgo/chapter06/Queue.java
@@ -0,0 +1,27 @@
+package com.michaelszymczak.training.grokalgo.chapter06;
+
+class Queue
+{
+
+    private boolean isEmpty = true;
+
+    public boolean isEmpty()
+    {
+        return isEmpty;
+    }
+
+    public int pop()
+    {
+        if (isEmpty)
+        {
+            throw new IllegalStateException("The queue is empty");
+        }
+        isEmpty = true;
+        return 0;
+    }
+
+    public void push(final int element)
+    {
+        isEmpty = false;
+    }
+}
--- src/test/java/com/michaelszymczak/training/grokalgo/chapter06/QueueTest.java
+++ src/test/java/com/michaelszymczak/training/grokalgo/chapter06/QueueTest.java
@@ -1,5 +1,7 @@
 package com.michaelszymczak.training.grokalgo.chapter06;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -7,24 +9,36 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class QueueTest
 {
+    private final Queue queue = new Queue();
+
     @Test
     void shouldBeEmptyInitially()
     {
-        Queue queue = new Queue();
         assertThat(queue.isEmpty()).isTrue();
         assertThatThrownBy(queue::pop).isInstanceOf(IllegalStateException.class);
     }
 
-    private static class Queue
+    @Nested
+    class GivenOneElement
     {
-        public boolean isEmpty()
+        @BeforeEach
+        void setUp()
         {
-            return true;
+            queue.push(5);
         }
 
-        public int pop()
+        @Test
+        void shouldNotBeEmpty()
         {
-            throw new IllegalStateException("The queue is empty");
+            assertThat(queue.isEmpty()).isFalse();
+        }
+
+        @Test
+        void shouldBeEmptyAfterRemovingTheElement()
+        {
+            queue.pop();
+            assertThat(queue.isEmpty()).isTrue();
         }
     }
+
 }
