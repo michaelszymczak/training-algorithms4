30b2403eb208acb5561de1cd252252824bc0a722
Michael Szymczak <contact@michaelszymczak.com>
2021-05-02 18:51:39 +0100

    #1 BFS: Refactor: remove init clause

--- src/test/java/com/michaelszymczak/training/grokalgo/chapter06/BFS.java
+++ src/test/java/com/michaelszymczak/training/grokalgo/chapter06/BFS.java
@@ -4,41 +4,37 @@ public class BFS
 {
     public boolean pathExists(final int[][] graph, final int startNode, final int endNode)
     {
-        if (graph.length < startNode + 1)
+        int nodeToCheck = startNode;
+        if (graph.length < nodeToCheck + 1)
         {
             return false;
         }
-        if (endNode == startNode)
-        {
-            return true;
-        }
+
         final Queue queue = new Queue(10);
         final Set set = new Set();
-        for (int i = 0; i < graph[startNode].length; i++)
+
+        while (true)
         {
-            queue.push(graph[startNode][i]);
-        }
-        while (!queue.isEmpty())
-        {
-            final int nodeToCheck = queue.pop();
             if (nodeToCheck == endNode)
             {
                 return true;
             }
-            else
+
+            for (int i = 0; i < graph[nodeToCheck].length; i++)
             {
-                for (int i = 0; i < graph[nodeToCheck].length; i++)
+                int nextNodeToCheck = graph[nodeToCheck][i];
+                if (!set.contains(nextNodeToCheck))
                 {
-                    int nextNodeToCheck = graph[nodeToCheck][i];
-                    if (!set.contains(nextNodeToCheck))
-                    {
-                        queue.push(nextNodeToCheck);
-                        set.add(nextNodeToCheck);
-                    }
+                    queue.push(nextNodeToCheck);
+                    set.add(nextNodeToCheck);
                 }
             }
-        }
 
-        return false;
+            if (queue.isEmpty())
+            {
+                return false;
+            }
+            nodeToCheck = queue.pop();
+        }
     }
 }
