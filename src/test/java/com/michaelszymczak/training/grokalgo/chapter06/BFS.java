becd0c96fcdad4b2d0d5909207d48d257222b9f9
Michael Szymczak <contact@michaelszymczak.com>
2021-05-02 17:52:12 +0100

    #1 BFS: Continue working on finding the shortest path

--- src/test/java/com/michaelszymczak/training/grokalgo/chapter06/BFS.java
+++ src/test/java/com/michaelszymczak/training/grokalgo/chapter06/BFS.java
@@ -2,12 +2,20 @@ package com.michaelszymczak.training.grokalgo.chapter06;
 
 public class BFS
 {
-    public int[] shortestPath(final int[][] graph, final int element)
+
+    public static final int[] NO_PATH = new int[0];
+
+    public int[] shortestPath(final int[][] graph, final int startNode, final int endNode)
     {
-        if (graph.length == 0)
+        if (graph.length < startNode + 1)
         {
-            return new int[0];
+            return NO_PATH;
         }
-        return new int[]{0};
+        if (endNode == startNode)
+        {
+            return new int[] {startNode};
+        }
+
+        return NO_PATH;
     }
 }
--- src/test/java/com/michaelszymczak/training/grokalgo/chapter06/BFSTest.java
+++ src/test/java/com/michaelszymczak/training/grokalgo/chapter06/BFSTest.java
@@ -9,7 +9,9 @@ public class BFSTest
     @Test
     void shouldShowEmptyPathWhenEmptyGraph()
     {
-        assertThat(new BFS().shortestPath(new int[0][0], 4)).isEqualTo(new int[0]);
+        assertThat(new BFS().shortestPath(new int[][] {
+
+        },0, 0)).isEqualTo(BFS.NO_PATH);
     }
 
     @Test
@@ -17,6 +19,14 @@ public class BFSTest
     {
         assertThat(new BFS().shortestPath(new int[][]{
                 new int[0]
-        }, 0)).isEqualTo(new int[]{0});
+        }, 0, 0)).isEqualTo(new int[]{0});
+    }
+
+    @Test
+    void shouldNotFindAnyPathIfTheSingleNodeDoesNotMatchTheSoughtOne()
+    {
+        assertThat(new BFS().shortestPath(new int[][]{
+                new int[0]
+        }, 1, 2)).isEqualTo(BFS.NO_PATH);
     }
 }
